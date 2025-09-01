5efc1100c1218165aef12b752f5a07151b179379
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-01 20:35:37 -0300

    Update tugProcessing.py

--- processamento/tugProcessing.py
+++ processamento/tugProcessing.py
@@ -35,81 +35,73 @@ def calcular_velocidade(acc, fs, metodo='savitzky', cutoff=5, ordem=3, janela=21
     return tempo, velocidade
 
 
-def processar_tug(df, filter_cutoff):
-    df_proc = df.copy()
-    time_vec = df_proc["Tempo"]
-    x = df_proc["X"]
-    y = df_proc["Y"]
-    z = df_proc["Z"]
-
-    # Escolher o eixo com maior variabilidade
-    v = x if np.std(x) > np.std(y) else y
+def processar_tug(df1,df2, filter_cutoff):
+    df_acc = df1.copy()
+    time_vec_acc = df_acc["Tempo"]
+    x_acc = df_acc["X"]
+    y_acc = df_acc["Y"]
+    z_acc = df_acc["Z"]
 
     # Reamostragem para 100 Hz
-    t_original = time_vec / 1000  # Converter para segundos
+    t_original = time_vec_acc / 1000  # Converter para segundos
     fs_novo = 100
     dt_novo = 1 / fs_novo
-    t_novo = np.arange(t_original.iloc[0], t_original.iloc[-1], dt_novo)
+    t_novo_acc = np.arange(t_original.iloc[0], t_original.iloc[-1], dt_novo)
 
     # Interpolação
-    interp_v = interp1d(t_original, v, kind='linear', fill_value="extrapolate")
-    v_interp = interp_v(t_novo)
+    interp_x_acc = interp1d(t_original, x_acc, kind='linear', fill_value="extrapolate")
+    interp_y_acc = interp1d(t_original, y_acc, kind='linear', fill_value="extrapolate")
+    interp_z_acc = interp1d(t_original, z_acc, kind='linear', fill_value="extrapolate")
+    z_acc_interp = interp_x_acc(t_novo_acc)
+    z_acc_interp = interp_y_acc(t_novo_acc)
+    z_acc_interp = interp_z_acc(t_novo_acc)
 
     # Remover tendência
-    #v_detrended = detrend(v_interp)
+    x_acc_detrended = detrend(x_acc_interp)
+    y_acc_detrended = detrend(y_acc_interp)
+    z_acc_detrended = detrend(z_acc_interp)
 
     # Filtragem passa-baixa
     nyquist = fs_novo / 2
     normal_cutoff = filter_cutoff / nyquist
     b, a = butter(4, normal_cutoff, btype='low', analog=False)
-    v_filtrado = filtfilt(b, a, v_interp)
+    x_acc_filtrado = filtfilt(b, a, x_acc_detrend)
+    y_acc_filtrado = filtfilt(b, a, y_acc_detrend)
+    z_acc_filtrado = filtfilt(b, a, z_acc_detrend)
+    norma_acc_filtrado = np.sqrt(x_acc_filtrado^2+y_acc_filtrado^2+z_acc_filtrado^2)
 
-    # Detecção do tempo de voo
-    limiar_vel = 0.5
-    indices_voo = np.where(np.abs(v_filtrado) > limiar_vel)[0]
+    df_gyro = df2.copy()
+    time_vec_gyro = df_acc["Tempo"]
+    x_gyro = df_gyro["X"]
+    y_gyro = df_gyro["Y"]
+    z_gyro = df_gyro["Z"]
 
-    altura = np.nan
-    inicio_voo_time = np.nan
-    fim_voo_time = np.nan
-    tempo_voo = np.nan
-    max_dec = np.nan
-    max_takeoff = np.nan
+    # Reamostragem para 100 Hz
+    t_original = time_vec_gyro / 1000  # Converter para segundos
+    fs_novo = 100
+    dt_novo = 1 / fs_novo
+    t_novo_gyro = np.arange(t_original.iloc[0], t_original.iloc[-1], dt_novo)
 
-    if len(indices_voo) >= 2:
-        min_mov = np.argmin(v_filtrado)
-        max_mov = np.argmax(v_filtrado)
+    # Interpolação
+    interp_x_gyro = interp1d(t_original, x_gyro, kind='linear', fill_value="extrapolate")
+    interp_y_gyro = interp1d(t_original, y_gyro, kind='linear', fill_value="extrapolate")
+    interp_z_gyro = interp1d(t_original, z_acc, kind='linear', fill_value="extrapolate")
+    z_gyro_interp = interp_x_acc(t_novo_gyro)
+    z_gyro_interp = interp_y_acc(t_novo_gyro)
+    z_gyro_interp = interp_z_acc(t_novo_gyro)
 
-        # Buscar início do voo (antes de min_mov)
-        try:
-            inicio_voo_idx = next(i for i in range(
-                min_mov-1, -1, -1) if v_filtrado[i] > 9.81)
-        except StopIteration:
-            inicio_voo_idx = 0
-        inicio_voo_time = t_novo[inicio_voo_idx]
-        max_dec = np.max(v_filtrado[0:inicio_voo_idx])
+    # Remover tendência
+    x_gyro_detrended = detrend(x_gyro_interp)
+    y_gyro_detrended = detrend(y_gyro_interp)
+    z_gyro_detrended = detrend(z_gyro_interp)
 
-        # Buscar fim do voo (antes de max_mov)
-        try:
-            fim_voo_idx = next(i for i in range(
-                max_mov-1, -1, -1) if v_filtrado[i] < 9.81)
-        except StopIteration:
-            fim_voo_idx = len(v_filtrado) - 1
-        fim_voo_time = t_novo[fim_voo_idx]
-        max_takeoff = np.max(v_filtrado[fim_voo_idx:])
-
-        tempo_voo = fim_voo_time - inicio_voo_time
-
-        # Estimar altura via fórmula balística
-        g = 9.81
-        altura = (g * (tempo_voo / 2)**2) / 2
-        velocidade_saida = (9.81*tempo_voo)/2
-
-        # Cálculo da velocidade
-        _, velocidade = calcular_velocidade(
-            v_filtrado[inicio_voo_idx:fim_voo_idx], fs_novo)
-
-        # Cálculo do deslocamento
-        deslocamento = cumulative_trapezoid(velocidade, dx=dt_novo, initial=0)
-        deslocamento = highpass_filter(deslocamento, fc=0.1, fs=fs_novo)
-
-    return t_novo, v_filtrado, inicio_voo_time, fim_voo_time, altura, tempo_voo, max_dec, max_takeoff, velocidade_saida, deslocamento, inicio_voo_idx, fim_voo_idx
+    # Filtragem passa-baixa
+    nyquist = fs_novo / 2
+    normal_cutoff = filter_cutoff / nyquist
+    b, a = butter(4, normal_cutoff, btype='low', analog=False)
+    x_gyro_filtrado = filtfilt(b, a, x_gyro_detrend)
+    y_gyro_filtrado = filtfilt(b, a, y_gyro_detrend)
+    z_gyro_filtrado = filtfilt(b, a, z_gyro_detrend)
+    norma_gyro_filtrado = np.sqrt(x_gyro_filtrado^2+y_gyro_filtrado^2+z_gyro_filtrado^2)
+    
+    return t_novo_acc, x_acc_filtrado, y_acc_filtrado, z_acc_filtrado, norma_acc_filtrado, t_novo_gyro, x_gyro_filtrado, y_gyro_filtrado, z_gyro_filtrado, norma_gyro_filtrado
