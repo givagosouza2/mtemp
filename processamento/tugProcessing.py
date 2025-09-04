03a3e6fef0c562981f39a48def2f2a191ffb684b
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-04 10:10:45 -0300

    Update tugProcessing.py

--- processamento/tugProcessing.py
+++ processamento/tugProcessing.py
@@ -115,6 +115,9 @@ def processar_tug(df1,df2,filter_cutoff1,filter_cutoff2):
         if valor > 0.25:
             stop_test = t_novo_gyro[index]
             break
+    idx_top = top2_peaks(v_gyro, x=t_novo_gyro, distance=None, prominence=None, abs_peaks=False)
+    st.text(idx_top)
+    
     duration = stop_test - start_test
     
     return t_novo_acc, x_acc_filtrado, y_acc_filtrado, z_acc_filtrado, norma_acc_filtrado, t_novo_gyro, v_gyro, ml_gyro, z_gyro_filtrado, norma_gyro_filtrado, start_test, stop_test
