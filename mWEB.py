7216378a09ea4eb6a4a793fb72c569e64781de7f
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-10 17:38:55 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -604,7 +604,7 @@ elif pagina == "📈 Visualização Gráfica":
                 else:
                     ax2_2.plot(
                     tempo_sel_2[startRec:endRec], ml_2_sel[startRec:endRec], color='black', linewidth=0.8)
-                    tempo_sel_2[startRec:endRec], ml_2_sel_media[startRec:endRec], color='blue', linewidth=0.8)
+                    ax2_2.plot(tempo_sel_2[startRec:endRec], ml_2_sel_media[startRec:endRec], color='blue', linewidth=0.8)
                     ax2_2.set_xlabel('Tempo (s)', fontsize=8)
                     ax2_2.set_ylabel(r'Aceleração ML (m/s$^2$)', fontsize=8)
                     ax2_2.set_xlim(-5, max(tempo))
@@ -617,7 +617,7 @@ elif pagina == "📈 Visualização Gráfica":
                     axv_2.plot(tempo_2, v_2, color='tomato', linewidth=0.5)
                 else: 
                     axv_2.plot(tempo_sel_2[startRec:endRec], v_2_sel[startRec:endRec], color='black', linewidth=0.8)
-                    tempo_sel_2[startRec:endRec], v_2_sel_media[startRec:endRec], color='blue', linewidth=0.8)
+                    ax2_2.plot(tempo_sel_2[startRec:endRec], v_2_sel_media[startRec:endRec], color='blue', linewidth=0.8)
                     axv_2.set_xlabel('Tempo (s)', fontsize=8)
                     axv_2.set_ylabel(r'Aceleração V (m/s$^2$)', fontsize=8)
                     axv_2.set_xlim(-5, max(tempo))
@@ -827,6 +827,7 @@ elif pagina == "📤 Exportar Resultados":
                 st.metric(label=r"Diferença de A2 e G4  (s)", value=round(A2_lat-G4_lat, 4))
                 
             
+
 
 
 
