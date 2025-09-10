ab38f8e416bf60f26393c12beaf3717ca7b5e8b8
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-09 23:24:15 -0300

    Update ytestProcessing.py

--- processamento/ytestProcessing.py
+++ processamento/ytestProcessing.py
@@ -7,18 +7,11 @@ from typing import Tuple
 def processar_ytest1(df, filter):
     # Aqui você pode aplicar filtros, normalizações, cálculo de deslocamentos etc.
     df_proc = df.copy()
-    df2_proc = df2.copy()
-        
+   
     time_vec = df_proc["Tempo"]
     x = df_proc["X"]
     y = df_proc["Y"]
     z = df_proc["Z"]
-
-    #joelho
-    time_vec_2 = df2_proc["Tempo"]
-    x_2 = df2_proc["X"]
-    y_2 = df2_proc["Y"]
-    z_2 = df2_proc["Z"]
 
     ap = z
     if np.mean(x) > np.mean(y):
@@ -70,61 +63,13 @@ def processar_ytest1(df, filter):
     ap_filtrado = filtfilt(b, a, ap_detrended)
     ml_filtrado = filtfilt(b, a, ml_detrended)
     v_filtrado = filtfilt(b, a, v_detrended)
-    
-    ap2 = x_2
-    v2 = y_2
-    ml2 = z_2
-
-    # Converter tempo para segundos
-    t_original_2 = time_vec_2 / 1000  # ms para s
-
-    # Criar vetor de tempo para 100 Hz
-    fs_novo = 100  # Hz
-    dt_novo = 1 / fs_novo
-    t_novo2 = np.arange(t_original_2.iloc[0], t_original_2.iloc[-1], dt_novo)
-
-    # Interpoladores
-    interp_ap2 = interp1d(t_original_2, ap2, kind='linear',
-                         fill_value="extrapolate")
-    interp_ml2 = interp1d(t_original_2, ml2, kind='linear',
-                         fill_value="extrapolate")
-    interp_v2 = interp1d(t_original_2, v2, kind='linear',
-                         fill_value="extrapolate")
-
-    # Sinais reamostrados
-    ap2_interp_100Hz = interp_ap2(t_novo2)
-    ml2_interp_100Hz = interp_ml2(t_novo2)
-    v2_interp_100Hz = interp_v2(t_novo2)
-
-    # 1. Remover tendência
-    ap2_detrended = detrend(ap2_interp_100Hz)
-    ml2_detrended = detrend(ml2_interp_100Hz)
-    v2_detrended = detrend(v2_interp_100Hz)
-
-    # 2. Filtro Butterworth passa-baixa com parâmetros da sidebar
-    fs = 100  # Hz
-    cutoff = filter  # Usando valor da sidebar
-    order = 4  # Usando valor da sidebar
-
-    # Normaliza a frequência de corte (Nyquist = fs/2)
-    nyquist = fs / 2
-    normal_cutoff = cutoff / nyquist
-
-    # Coeficientes do filtro
-    b, a = butter(order, normal_cutoff, btype='low', analog=False)
-
-    # Aplicando o filtro com zero-phase (filtfilt)
-    ap2_filtrado = filtfilt(b, a, ap2_detrended)
-    ml2_filtrado = filtfilt(b, a, ml2_detrended)
-    v2_filtrado = filtfilt(b, a, v2_detrended)
-        
+            
     return t_novo, ml_filtrado, ap_filtrado, v_filtrado
     
 def processar_ytest2(df, filter):
     # Aqui você pode aplicar filtros, normalizações, cálculo de deslocamentos etc.
     df_proc = df.copy()
-    df2_proc = df2.copy()
-        
+   
     time_vec = df_proc["Tempo"]
     x = df_proc["X"]
     y = df_proc["Y"]
