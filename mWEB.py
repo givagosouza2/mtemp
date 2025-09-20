a42753e8635d3a980ee4f38cb057b107917965eb
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-20 11:56:09 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -53,8 +53,9 @@ pagina = st.sidebar.radio("📂 Navegue pelas páginas", [
 # === Página Inicial ===
 if pagina == "🏠 Página Inicial":
     
-    st.write(
-        "Bem-vindo ao Momentum Web, o web app para análise de dados de protocolos de avaliação do Momentum Sensors. Os protocolos de análise dos dados são baseados em métodos usados em artigos científicos do grupo idealizador do Projeto Momentum. O Projeto Momentum representa uma iniciativa de desenvolvimento científico e tecnológico com o objetivo de propor métodos confiáveis de avaliação sensório-motora usando sensores presentes em smartphones. Alguns protocolos estarão em desenvolvimento e indicados quando for o caso. Utilize o menu lateral para navegar entre as diferentes etapas da análise.")
+    st.H2(
+        "Bem-vindo ao Momentum Web, o web app para análise de dados de protocolos de avaliação do Momentum Sensors")
+    st.markdown(Os protocolos de análise dos dados são baseados em métodos usados em artigos científicos do grupo idealizador do Projeto Momentum. O Projeto Momentum representa uma iniciativa de desenvolvimento científico e tecnológico com o objetivo de propor métodos confiáveis de avaliação sensório-motora usando sensores presentes em smartphones. Alguns protocolos estarão em desenvolvimento e indicados quando for o caso. Utilize o menu lateral para navegar entre as diferentes etapas da análise.")
 
 # === Página de Importação ===
 elif pagina == "📁 Importar Dados":
@@ -1132,6 +1133,7 @@ elif pagina == "📤 Exportar Resultados":
                 mime="text/plain"
             )        
             
+
 
 
 
