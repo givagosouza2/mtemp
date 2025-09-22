5f718084ebb0c547d9007a79c14707ff647588fe
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-22 08:36:31 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -90,7 +90,7 @@ elif pagina == "⬆️ Importar Dados":
                 if dados is not None:
                     st.success('Dados carregados com sucesso')
                     st.session_state["dados"] = dados
-         elif tipo_teste == "Salto":
+        elif tipo_teste == "Salto":
              st.subheader("🤸 Importar dados de Salto")
              arquivo = st.file_uploader( "Selecione o arquivo de salto (CSV ou TXT)", type=["csv", "txt"])
              if arquivo is not None:
@@ -935,6 +935,7 @@ elif pagina == "📖 Referências bibliográficas":
     <a href="https://www.scielo.br/j/aabc/a/7z5HDVZKYVMxfWm8HxcJqZG/?lang=en&format=pdf" target="_blank" style="color:#1E90FF; text-decoration:none;">15. ALMEIDA, J. R. ; MONTEIRO, L. C. P. ; SOUZA, P. H. C. ; ANDRÉ DOS SANTOS, CABRAL ; BELGAMO, A. ; COSTA E SILVA, A. A ; CRISP, A. ; CALLEGARI, B. ; AVILA, P. E. S. ; SILVA, J. A. ; BASTOS, G. N. T. ; SOUZA, G.S. . Comparison of joint position sense measured by inertial sensors embedded in portable digital devices with different masses. Frontiers in Neuroscience, v. 19, p. 1-1, 2025.</a>.</p> 
     </p> </div> """)
     st.markdown(html, unsafe_allow_html=True)
+
 
 
 
