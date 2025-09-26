d0cf4a404a3a18cc0d31a6f864269e5102b9dcec
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-26 18:34:17 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -501,6 +501,12 @@ elif pagina == "📈 Visualização Gráfica":
                 if valor == min_c1:
                     t_min_c1 = tempo_sel[index]
                     break
+            for index2,valor in enumerate(tempo_sel_2):
+                if valor == t_min_c1:
+                    break
+            t1_ap_minus = tempo_sel_2[index2-50]
+            t1_ap_plus = tempo_sel_2[index2]
+            
             max_c1 = np.max(ap_sel_media[index:index+1000])
             for index,valor in enumerate(ap_sel_media):
                 if valor == max_c1:
@@ -569,6 +575,7 @@ elif pagina == "📈 Visualização Gráfica":
                 ax1_2.plot([t_max_c1,t_max_c1],[-4,4],"--r")
                 ax1_2.plot([t_min_c2,t_min_c2],[-4,4],"--r")
                 ax1_2.plot([t_max_c2,t_max_c2],[-4,4],"--r")
+                ax1_2.plot([t1_ap_minus,t1_ap_minus,t1_ap_plus,t1_ap_plus],[-2,2,-2,2],"-k")
                 ax1_2.set_xlabel(r'Tempo (s)', fontsize=8)
                 ax1_2.set_ylabel(r'Aceleração AP (m/s$^2$)', fontsize=8)
                 ax1_2.set_xlim(-5, limite_tempo)
@@ -1066,6 +1073,7 @@ elif pagina == "📖 Referências bibliográficas":
     <a href="https://www.scielo.br/j/aabc/a/7z5HDVZKYVMxfWm8HxcJqZG/?lang=en&format=pdf" target="_blank" style="color:#1E90FF; text-decoration:none;">15. ALMEIDA, J. R. ; MONTEIRO, L. C. P. ; SOUZA, P. H. C. ; ANDRÉ DOS SANTOS, CABRAL ; BELGAMO, A. ; COSTA E SILVA, A. A ; CRISP, A. ; CALLEGARI, B. ; AVILA, P. E. S. ; SILVA, J. A. ; BASTOS, G. N. T. ; SOUZA, G.S. . Comparison of joint position sense measured by inertial sensors embedded in portable digital devices with different masses. Frontiers in Neuroscience, v. 19, p. 1-1, 2025.</a>.</p> 
     </p> </div> """)
     st.markdown(html, unsafe_allow_html=True)
+
 
 
 
