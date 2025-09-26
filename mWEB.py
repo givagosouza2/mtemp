05ae5129db6e1fae9ee375c4e59c846486c2c0c5
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-09-26 17:48:59 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -851,12 +851,13 @@ elif pagina == "📤 Exportar Resultados":
                 limite_tempo = n1
             else:
                 limite_tempo = n2
+            
             min_c1 = np.min(ap_sel_media[startRec:endRec])
             for index,valor in enumerate(ap_sel_media):
                 if valor == min_c1:
                     t_min_c1 = tempo_sel[index]
                     break
-            for index,valor in enumerate(tempo_sel_2)
+            for index,valor in enumerate(tempo_sel_2):
                 if valor == t_min_c1:
                     break
             dados = ap_2_sel_media[index-50:index]        
@@ -869,7 +870,7 @@ elif pagina == "📤 Exportar Resultados":
                 if valor == max_c1:
                     t_max_c1 = tempo_sel[index]
                     break
-            for index,valor in enumerate(tempo_sel_2)
+            for index,valor in enumerate(tempo_sel_2):
                 if valor == t_max_c1:
                     break
             dados = ap_2_sel_media[index-50:index]        
@@ -882,7 +883,7 @@ elif pagina == "📤 Exportar Resultados":
                 if valor == min_c2:
                     t_min_c2 = tempo_sel[index]
                     break
-            for index,valor in enumerate(tempo_sel_2)
+            for index,valor in enumerate(tempo_sel_2):
                 if valor == t_min_c2:
                     break
             dados = ap_2_sel_media[index-50:index]        
@@ -895,7 +896,7 @@ elif pagina == "📤 Exportar Resultados":
                 if valor == max_c2:
                     t_max_c2 = tempo_sel[index]
                     break
-            for index,valor in enumerate(tempo_sel_2)
+            for index,valor in enumerate(tempo_sel_2):
                 if valor == t_max_c2:
                     break        
             dados = ap_2_sel_media[index-50:index]        
@@ -1029,6 +1030,7 @@ elif pagina == "📖 Referências bibliográficas":
     <a href="https://www.scielo.br/j/aabc/a/7z5HDVZKYVMxfWm8HxcJqZG/?lang=en&format=pdf" target="_blank" style="color:#1E90FF; text-decoration:none;">15. ALMEIDA, J. R. ; MONTEIRO, L. C. P. ; SOUZA, P. H. C. ; ANDRÉ DOS SANTOS, CABRAL ; BELGAMO, A. ; COSTA E SILVA, A. A ; CRISP, A. ; CALLEGARI, B. ; AVILA, P. E. S. ; SILVA, J. A. ; BASTOS, G. N. T. ; SOUZA, G.S. . Comparison of joint position sense measured by inertial sensors embedded in portable digital devices with different masses. Frontiers in Neuroscience, v. 19, p. 1-1, 2025.</a>.</p> 
     </p> </div> """)
     st.markdown(html, unsafe_allow_html=True)
+
 
 
 
