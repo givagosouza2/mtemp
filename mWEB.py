05a7bdaf463fe996e0f74f85b58118f2bb636178
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-12-25 14:21:50 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -239,8 +239,8 @@ elif pagina == "📈 Visualização Gráfica":
                     default_cutoff = min(10.0, nyquist - 0.1)
                     cutoff = st.number_input(
                         "Frequência de corte (Hz)",
-                        min_value=0.1,
-                        max_value=float(nyquist - 0.01),
+                        min=0.1,
+                        max=float(nyquist - 0.01),
                         value=default_cutoff,
                         step=0.1
                     )
@@ -1209,6 +1209,7 @@ elif pagina == "📖 Referências bibliográficas":
     <a href="https://www.scielo.br/j/aabc/a/7z5HDVZKYVMxfWm8HxcJqZG/?lang=en&format=pdf" target="_blank" style="color:#1E90FF; text-decoration:none;">15. ALMEIDA, J. R. ; MONTEIRO, L. C. P. ; SOUZA, P. H. C. ; ANDRÉ DOS SANTOS, CABRAL ; BELGAMO, A. ; COSTA E SILVA, A. A ; CRISP, A. ; CALLEGARI, B. ; AVILA, P. E. S. ; SILVA, J. A. ; BASTOS, G. N. T. ; SOUZA, G.S. . Comparison of joint position sense measured by inertial sensors embedded in portable digital devices with different masses. Frontiers in Neuroscience, v. 19, p. 1-1, 2025.</a>.</p> 
     </p> </div> """)
     st.markdown(html, unsafe_allow_html=True)
+
 
 
 
