f1cd0278cddfd0e067b4619f7303e81860ed4da5
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2025-12-25 14:51:15 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -215,7 +215,7 @@ elif pagina == "📈 Visualização Gráfica":
         if tipo_teste == "Registro inercial livre":
             dados = st.session_state["dados"]
             t= dados["Tempo"]
-            t = t*1000
+            t = t/1000
             x = dados["X"]
             y = dados["Y"]
             z = dados["Z"]
@@ -238,6 +238,7 @@ elif pagina == "📈 Visualização Gráfica":
                         dt = np.diff(t)
                         fs_mean = 1 / np.mean(dt)
                         nyquist = fs_mean / 2
+                        st.text(nyquist)
                     
                         # garante limites válidos (evita nyquist muito baixo)
                         min_cutoff = 0.1
@@ -1219,6 +1220,7 @@ elif pagina == "📖 Referências bibliográficas":
     <a href="https://www.scielo.br/j/aabc/a/7z5HDVZKYVMxfWm8HxcJqZG/?lang=en&format=pdf" target="_blank" style="color:#1E90FF; text-decoration:none;">15. ALMEIDA, J. R. ; MONTEIRO, L. C. P. ; SOUZA, P. H. C. ; ANDRÉ DOS SANTOS, CABRAL ; BELGAMO, A. ; COSTA E SILVA, A. A ; CRISP, A. ; CALLEGARI, B. ; AVILA, P. E. S. ; SILVA, J. A. ; BASTOS, G. N. T. ; SOUZA, G.S. . Comparison of joint position sense measured by inertial sensors embedded in portable digital devices with different masses. Frontiers in Neuroscience, v. 19, p. 1-1, 2025.</a>.</p> 
     </p> </div> """)
     st.markdown(html, unsafe_allow_html=True)
+
 
 
 
