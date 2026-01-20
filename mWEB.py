d174e45cc2484e91376e88ef84765426bf1e46a0
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-01-20 17:18:45 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -1012,18 +1012,18 @@ elif pagina == "📤 Exportar Resultados":
                 st.metric(label=r"Amplitude pós-C2 Joelho ML (m/s2)", value=round(ampC2_ml_pos, 4))  
                 
             with col4: 
-                st.metric(label=r"Amplitude de C4 (m/s2)", value=round(max_c2, 4)) 
-                st.metric(label=r"Tempo de C4 (s)", value=round(t_max_c2, 4)) 
-                st.metric(label=r"Amplitude pré-C4 Joelho AP (m/s2)", value=round(ampC4_ap_pre, 4)) 
-                st.metric(label=r"Amplitude pós-C4 Joelho AP (m/s2)", value=round(ampC4_ap_pos, 4)) 
-                st.metric(label=r"Amplitude pré-C4 Joelho ML (m/s2)", value=round(ampC4_ml_pre, 4)) 
-                st.metric(label=r"Amplitude pós-C4 Joelho ML (m/s2)", value=round(ampC4_ml_pos, 4)) 
+                st.metric(label=r"Amplitude de C3 (m/s2)", value=round(max_c2, 4)) 
+                st.metric(label=r"Tempo de C3 (s)", value=round(t_max_c2, 4)) 
+                st.metric(label=r"Amplitude pré-C3 Joelho AP (m/s2)", value=round(ampC4_ap_pre, 4)) 
+                st.metric(label=r"Amplitude pós-C3 Joelho AP (m/s2)", value=round(ampC4_ap_pos, 4)) 
+                st.metric(label=r"Amplitude pré-C3 Joelho ML (m/s2)", value=round(ampC4_ml_pre, 4)) 
+                st.metric(label=r"Amplitude pós-C3 Joelho ML (m/s2)", value=round(ampC4_ml_pos, 4)) 
 
             variaveis = [("Amplitude de C1 (m/s2)", round(min_c1, 4)), ("Tempo de C1 (s)", round(t_min_c1, 4)), ("Amplitude de C2 (m/s2)", round(max_c1, 4)), ("Tempo de C2 (s)", round(t_max_c1, 4)), ("Amplitude de C3 (m/s2)", round(max_c2, 4)), ("Tempo de C3 (s)", round(t_max_c2, 4)), ("Amplitude pré-C1 Joelho AP (m/s2)",round(ampC1_ap_pre, 4)), ("Amplitude pós-C1 Joelho AP (m/s2)",round(ampC1_ap_pos, 4)), ("Amplitude pré-C2 Joelho AP (m/s2)",round(ampC2_ap_pre, 4)), ("Amplitude pós-C2 Joelho AP (m/s2)",round(ampC2_ap_pos, 4)), ("Amplitude pré-C4 Joelho AP (m/s2)",round(ampC4_ap_pre, 4)), ("Amplitude pós-C4 Joelho AP (m/s2)",round(ampC4_ap_pos, 4)), ("Amplitude pré-C1 Joelho ML (m/s2)",round(ampC1_ml_pre, 4)), ("Amplitude pós-C1 Joelho ML (m/s2)",round(ampC1_ml_pos, 4)), ("Amplitude pré-C2 Joelho ML (m/s2)",round(ampC2_ml_pre, 4)), ("Amplitude pós-C2 Joelho ML (m/s2)",round(ampC2_ml_pos, 4)), ("Amplitude pré-C4 Joelho ML (m/s2)",round(ampC4_ml_pre, 4)), ("Amplitude pós-C4 Joelho ML (m/s2)",round(ampC4_ml_pos, 4)),] 
             resultado_txt = "Variável\tValor\n" # Cabeçalho com tabulação 
             for nome, valor in variaveis: 
                 resultado_txt += f"{nome}\t{valor}\n" 
-                st.download_button( label="📄 Exportar resultados (.txt)", data=resultado_txt, file_name="resultados_propriocepcao.txt", mime="text/plain" )
+            st.download_button( label="📄 Exportar resultados (.txt)", data=resultado_txt, file_name="resultados_propriocepcao.txt", mime="text/plain" )
         if tipo_teste == "Propriocepção": 
             calibracao = st.session_state["calibracao"] 
             dados = st.session_state["dados"] 
@@ -1093,6 +1093,7 @@ elif pagina == "📖 Referências bibliográficas":
     </div>
     """)
     st.markdown(html, unsafe_allow_html=True)
+
 
 
 
