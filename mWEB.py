8bbae52afedf914bf54c31c206340e3d5fe29f55
givagosouza2 <147889314+givagosouza2@users.noreply.github.com>
2026-03-24 17:58:54 -0300

    Update mWEB.py

--- mWEB.py
+++ mWEB.py
@@ -809,9 +809,28 @@ elif pagina == "📤 Exportar Resultados":
 
                 tempo_sel, ml_sel, ap_sel, freqs_sel, psd_ml_sel, psd_ap_sel = \
                     balanceProcessing.processar_equilibrio(dados, startRec, endRec, 1, 0, fc)
-                st.text(freqs_sel)
+                
+                LF_power_ml = trapezoid(psd_ml_sel[0:4], freqs_sel[0:4])
+                LF_power_ap = trapezoid(psd_ap_sel[0:4], freqs_sel[0:4])
+                
+                MF_power_ml = trapezoid(psd_ml_sel[5:19], freqs_sel[5:19])
+                MF_power_ap = trapezoid(psd_ap_sel[5:19], freqs_sel[5:19])
+
+                HF_power_ml = trapezoid(psd_ml_sel[20:79], freqs_sel[20:79])
+                HF_power_ap = trapezoid(psd_ap_sel[20:79], freqs_sel[20:79])
+                
                 total_power_ml = trapezoid(psd_ml_sel, freqs_sel)
                 total_power_ap = trapezoid(psd_ap_sel, freqs_sel)
+
+                for index, val in enumerate(psd_ml_sel):
+                    if np.sum(psd_ml_sel[0:index]) > total_power_ml/2:
+                        freq_median_ml = freqs_sel[index-1]
+                        break
+
+                for index, val in enumerate(psd_ap_sel):
+                    if np.sum(psd_ap_sel[0:index]) > total_power_ap/2:
+                        freq_median_ap = freqs_sel[index-1]
+                        break
 
                 resultado_txt = "Variável\tValor\n"
                 variaveis = [
@@ -821,6 +840,14 @@ elif pagina == "📤 Exportar Resultados":
                     ("Área elipse", round(ellipse_area, 6)),
                     ("Potência total ML", round(total_power_ml, 10)),
                     ("Potência total AP", round(total_power_ap, 10)),
+                    ("Potência Baixa Freq ML", round(LF_power_ml, 10)),
+                    ("Potência Baixa Freq AP", round(LF_power_ap, 10)),
+                    ("Potência Média Freq ML", round(MF_power_ml, 10)),
+                    ("Potência Média Freq AP", round(MF_power_ap, 10)),
+                    ("Potência Alta Freq ML", round(HF_power_ml, 10)),
+                    ("Potência Alta Freq AP", round(HF_power_ml, 10)),
+                    ("Frequência mediana ML", round(freq_median_ml, 10)),
+                    ("Frequência mediana AP", round(freq_median_ap, 10)),
                 ]
                 for nome, valor in variaveis:
                     resultado_txt += f"{nome}\t{valor}\n"
